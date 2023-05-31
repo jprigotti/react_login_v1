5224af83fed0bc79a2b96f259fa86aaee000dc02
Informatica Departamento <informatica@santojanni.gcba.gov.ar>
2023-05-31 13:00:23 -0300

    added login auth

--- src/pages/login/views/LoginView.jsx
+++ src/pages/login/views/LoginView.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
 import LoginForm from '../components/LoginForm'
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../hooks/useAuth";
+
 
 const LoginView = () => {
 
     const navigate = useNavigate();
-    
+    const { login, loginWithGoogle } = useAuth();
+     
+
     const handleRegistration = () => {
         navigate("/register",
             { replace: true });
+    }
+
+    const handleLoginGoogle = async () => {
+        try{    
+            const response = await loginWithGoogle();
+            console.log("signin response ", response);
+            login(response.user.email);
+            navigate("/", { replace: true });
+        }catch(error){
+            console.log(error);
+        }
+
     }
 
     return (
         <div>
             <h3>Sign In</h3>
             <LoginForm />
+            <button onClick={handleLoginGoogle}>Login with Google</button>
             <p>Don't have an account yet? Register </p>
             <button onClick={handleRegistration}>Here</button>
 
